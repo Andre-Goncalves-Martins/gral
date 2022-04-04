9d258a5688afc6534a6e9fd813dffb2cd935d10e
Andre-Goncalves-Martins <andregmartins26@gmail.com>
2022-04-04 07:55:58 -0300

    nada

--- hospital_backend/settings.py
+++ hospital_backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-r-%$p8_y!%u*2ecv=)qg3y2j$wq^absecu+7%(jm#!usysllpj'#os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
